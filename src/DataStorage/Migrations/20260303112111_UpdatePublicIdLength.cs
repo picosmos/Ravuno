759c90d8dc84bafd36c802be1c7a3123ac5a74ce
Tom Lambert (picosmos) <code@picosmos.de>
2026-03-03 12:23:12 +0100

    fixed database

---
+++ src/DataStorage/Migrations/20260303112111_UpdatePublicIdLength.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ravuno.DataStorage.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePublicIdLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Queries",
+                type: "TEXT",
+                maxLength: 32,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Queries",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 32,
+                oldCollation: "NOCASE"
+            );
+        }
+    }
+}
